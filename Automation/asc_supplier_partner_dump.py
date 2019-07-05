53af2e9814f30d9f40e2042ae50d4c3e8ed3f742
alaskar <asfkol@gmail.com>
2019-07-05 14:13:10 +0530

    moved username and password to command line. added checks for number of arguments.

--- Automation/asc_supplier_partner_dump.py
+++ Automation/asc_supplier_partner_dump.py
@@ -2,7 +2,6 @@ import xlwt
 import os
 import sys
 import cx_Oracle
-# import db_connection as dc
 from datetime import datetime
 import pytz
 import traceback
@@ -22,7 +21,6 @@ normal_style = xlwt.easyxf('font: height 240, name Arial, colour_index black, bo
 '''
 
 
-
 class InvalidBrand(Exception):
     """ This is a custom exception """
     pass
@@ -34,13 +32,25 @@ def send_mail(*args):
         print('Email {}'.format(arg))
 
 
+def zip_files():
+    pass
+
+
+def usage():
+    print('Number of arguments {0}'.format(len(sys.argv)))
+    if len(sys.argv) != 3:
+        print('Usage: {0} {1} {2}'.format(sys.argv[0], 'username', 'password'))
+        sys.exit(1)
+
+
 def make_db_connection(brand):
 
     # Take input from standard input.
-    # user = sys.argv[1]
-    # password = sys.argv[2]
-    user = 'alaskar_db'
-    password = 'Ascena2020!'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    # print('Number of arguments {0}'.format(len(sys.argv)))
+    # user = 'alaskar_db'
+    # password = 'Ascena2020!'
     lb_connectionstring = cx_Oracle.makedsn('ORAPRDCSTR01-SCAN.corp.local', 1521, service_name='RMSPRD.corp.local')
     ca_connectionstring = cx_Oracle.makedsn('ORAPRDCSTR01-SCAN.corp.local', 1521, service_name='RMSPRCA.corp.local')
     drs_connectionstring = cx_Oracle.makedsn('dbrmsproddb', 1521, 'RMSPRD')
@@ -174,10 +184,13 @@ def write_excel(brand):
 
 
 if __name__ == '__main__':
-    # write_excel('LB')
+
+    # check parameter passed.
+    usage()
+    write_excel('LB')
     write_excel('MAU')
-    # write_excel('DRS')
-    # write_excel('CA')
+    write_excel('DRS')
+    write_excel('CA')
 
 
 
